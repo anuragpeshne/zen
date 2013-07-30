361566552c43bdf4ff9b8abd5773ded1bbbd9ad7
Anurag Peshne <anurag_peshne@yahoo.co.in>
2013-07-30 11:10:14 +0530

    removed comments

--- zenReader.py
+++ zenReader.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt4 tutorial 
-
-In this example, we dispay an image
-on the window. 
-
-author: Jan Bodnar
-website: zetcode.com 
-last edited: September 2011
-"""
-
 import sys
 from PyQt4 import QtGui, QtCore
 import os
